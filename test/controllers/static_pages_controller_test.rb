d96b5d8d659dac9bcae810ba9833e24db8675856
Jason C Miller <jsonprime@gmail.com>
2015-04-25 14:13:06 -0600

    Add a Contact page (solves exercise 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | #{@title_description}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@title_description}"
+  end
+
 end

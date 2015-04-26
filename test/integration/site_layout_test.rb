fea989bda8460f9b223c1b9a1c7ef2e1c1f65e54
Jason C Miller <jsonprime@gmail.com>
2015-04-26 17:29:09 -0600

    Added another integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+	def setup
+	    @title_description = "Ruby on Rails Tutorial Sample App"
+	end
+
 	test "layout links" do 
 		get root_path
 		assert_template 'static_pages/home'
@@ -14,6 +18,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 	test "signup page links" do
 		get signup_path
 		assert_template 'users/new'
+		assert_select "title", "Sign Up | #{@title_description}"
 	end
 
 end

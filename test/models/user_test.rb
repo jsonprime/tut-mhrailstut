ecbcdd07bfeca72c3f13abb08ac326ec0391e8b4
Jason C Miller <jsonprime@gmail.com>
2015-04-27 11:51:54 -0600

    Added email downcase test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,4 +61,11 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
+	test "email should be saved in lower case" do
+		mixed_case_email = "Foo@ExaMPLe.cOM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
 end

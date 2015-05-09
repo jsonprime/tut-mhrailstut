5b61a9e6aac0f42e459f7cd27c30e2f4d5ff28f1
Jason C Miller <jsonprime@gmail.com>
2015-05-09 13:15:30 -0600

    Added passing integration tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,15 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
+	# Returns true if the user is logged-in, otherwise false
+	def logged_in?
+		!current_user.nil?
+	end
+
+	# Logs out the current user
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 end

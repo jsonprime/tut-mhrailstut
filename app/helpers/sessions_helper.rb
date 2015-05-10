afbaa860ca1f06b21b2dc075c934649e736e17a7
Jason C Miller <jsonprime@gmail.com>
2015-05-10 17:07:25 -0600

    Added cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,15 @@ module SessionsHelper
 
 	# Returns the current logged-in user (if any).
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		elseif cookies.signed[:user_id]
+			user = User.find_by(id: cookies.signed[:user_id])
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user_id
+				@current_user = user
+			end
+		end
 	end
 
 	# Returns true if the user is logged-in, otherwise false
@@ -19,6 +27,13 @@ module SessionsHelper
 	def log_out
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	# Remembers a user in a persistent session
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
 	end
 
 end

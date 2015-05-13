bfcc826d44b9286a90575a744001ec71b7ad85b8
Jason C Miller <jsonprime@gmail.com>
2015-05-13 12:07:13 -0600

    Fixed issue of passing in user id instead of user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,9 +2,7 @@ module SessionsHelper
 
 	# Logs  in the given user
 	def log_in(user)
-		if user != nil
-			session[:user_id] = user.id
-		end
+		session[:user_id] = user.id
 	end
 
 	# Returns the current logged-in user (if any).
@@ -14,7 +12,7 @@ module SessionsHelper
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
 			if user && user.authenticated?(cookies[:remember_token])
-				log_in user_id
+				log_in user
 				@current_user = user
 			end
 		end
